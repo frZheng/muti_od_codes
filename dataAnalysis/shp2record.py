9d1f922bfa036c8ddcd95121ed4203d6c2d83f85
frZheng <fr_zheng@163.com>
2022-07-19 14:27:15 +0800

    commit

--- dataAnalysis/shp2record.py
+++ dataAnalysis/shp2record.py
@@ -151,40 +151,131 @@ def writeMarginFile():
     sf = shapefile.Reader(
         r"C:\Users\Administrator\Documents\ArcGIS\mySave\test\sz_margin.shp")
 
-    shapes = sf.shapes()
-    max_area_index = 0
-    for i in range(0, len(shapes)):
-    print(shapes)
+    # def getArea(geolocations):
+    #     area = 0  # 多边形面积
+    #     lenth = len(geolocations)
+    #     for i in range(1, lenth + 1, 1):
+    #         iLat = geolocations[i % lenth][1]
+    #         iLng = geolocations[i % lenth][0]
+    #         nextLat = geolocations[i - 1][1]
+    #         nextLng = geolocations[i - 1][0]
+    #         temp = abs(iLat * nextLng - iLng * nextLat) / 2.0
+    #         area += temp
+    #
+    #     return area
 
-    exit()
+    import numpy as np
+
+    """
+        计算多边形的面积，包括非凸多边形的情况，
+        要求输入的顶点是按照逆时针顺序顺次排列的。
+        所要求解面积的多边形即为这些顶点一个一个地相连而成
+        E = {<v0, v1>, <v1, v2>, <v2, v3>,...,<vn-2, vn-1>, <vn-1, v0>}
+        @author: sdu_brz
+        @date: 2019/02/18
+    """
+
+    def coss_multi(v1, v2):
+        """
+        计算两个向量的叉乘
+        :param v1:
+        :param v2:
+        :return:
+        """
+        return v1[0] * v2[1] - v1[1] * v2[0]
+
+    def getArea(polygon):
+        """
+        计算多边形的面积，支持非凸情况
+        :param polygon: 多边形顶点，已经进行顺次逆时针排序
+        :return: 该多边形的面积
+        """
+        n = len(polygon)
+
+        if n < 3:
+            return 0
+
+        vectors = np.zeros((n, 2))
+        for i in range(0, n):
+            vectors[i, :] = polygon[i, :] - polygon[0, :]
+
+        area = 0
+        for i in range(1, n):
+            area = area + coss_multi(vectors[i - 1, :], vectors[i, :]) / 2
+
+        return abs(area)
+
+
+    shapes = sf.shapes()
+    print(shapes)
+    max_area_index = 0
+    max_area = 0
+    for i in range(0, len(shapes)):
+        points = []
+        for j in range(len(shapes[i].points)):
+            points.append([shapes[i].points[j][0], shapes[i].points[j][1]])
+        area = getArea(np.array(points))
+        print(i,area)
+        if max_area < area:
+            max_area_index = i
+            max_area = area
+        # area = len(shapes[i].points)
+        # if max_area < area:
+        #     max_area_index = i
+        #     max_area = area
+    print(max_area_index,max_area)
+
+    # exit()
     # 格式 序号,lon1,lat1,,lon2,lat2,...,lonn,latn,
     with open("data/sz_margin_GCJ02_v1.csv", "w") as fout:
-        for i in range(0, len(shapes)):
-            arr.append(shapes[i].bbox)
-            # print(i,len(datas.points))
-            msg = ""
-            msg += str(i) + ","
 
-            points = []
-            for j in range(len(shapes[i].points)):
-                points.append([shapes[i].points[j][0],shapes[i].points[j][1]])
-            # center = center_geolocation(points) # V1 的中心计算方式
-            # center = getCenterOfGravityPoint(points) # V2 的计算方式
-            from geo_utile import wgs84togcj02
-            center = wgs84togcj02(float(centers[i][0]),float(centers[i][1])) # 按照argis生成的中心
-            msg += str(center[0]) + ","
-            msg += str(center[1]) + ","
+        from geo_utile import wgs84togcj02
 
-            for j in range(len(shapes[i].bbox)):
-                msg += str(shapes[i].bbox[j]) + ","
-            # 能按照顺序连接成框
-            for j in range(len(shapes[i].points)):
-                [lng,lat] = wgs84togcj02(float(shapes[i].points[j][0]),float(shapes[i].points[j][1]))
-                msg += str(lng) + "," + str(lat) + ","
-            msg = msg[:-1]  # 去掉最后一个逗号
-            fout.write(msg + "\n")
+        msg = ""
 
-writeMarginFile()
+        # 能按照顺序连接成框
+        for j in range(len(shapes[max_area_index].points)):
+            [lng,lat] = wgs84togcj02(float(shapes[max_area_index].points[j][0]),float(shapes[max_area_index].points[j][1]))
+            msg += str(lng) + "," + str(lat) + ","
+        msg = msg[:-1]  # 去掉最后一个逗号
+        fout.write(msg + "\n")
+
+    import matplotlib.pyplot as plt
+    RedionData = []
+    # with open("data/sz_points_GCJ02_test.csv", "r") as fin:
+    with open("data/sz_margin_GCJ02_v1.csv", "r") as fin:
+        lines = fin.readlines()
+        for line in lines:
+            if line[-1] == "\n":
+                line = line[:-1]
+            RedionData.append(line.split(","))
+
+    for k in range(len(RedionData)):#绘制整个城市的区域
+        y_arr = []
+        x_arr = []
+        for j in range(0, len(RedionData[k]), 2):
+            # print(j)
+            x_arr.append(float(RedionData[k][j]))
+            y_arr.append(float(RedionData[k][j + 1]))
+
+        plt.plot(x_arr, y_arr,color="k",linewidth=0.1)
+    center = [[113.897469,22.457764],#SCT行政大楼
+              [114.531248,22.462121] ,#日出观景台
+              [114.587138,22.655723],# 红排角
+              [113.954324,22.505089],#弯月山谷-观景平台
+              [113.952761,22.488265], #深圳观桥公园
+              [113.916166,22.476449], #深圳湾游艇会大楼
+              ]
+
+
+    for i in range(len(center)):
+        # [lng,lat] = gcj02towgs84(center[i][0], center[i][1])
+        lng, lat = center[i][0], center[i][1]
+        plt.scatter(lng,lat, color='r',s=0.1)
+    # plt.savefig("pdf/test.pdf")
+    plt.show()
+
+# writeMarginFile()
 
 
 # def writeCenterFile():
