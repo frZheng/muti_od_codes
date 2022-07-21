3fe0598e42ff98ac6e597e3f13bb6e7ebaf112bb
Zheng Fu Rong <92928083+frZheng@users.noreply.github.com>
2022-07-21 09:26:04 +0800

    Delete dataAnalysis/shell2 directory

--- dataAnalysis/shell2/putJars.sh
+++
@@ -1,4 +0,0 @@
-hadoop fs -rm -r /user/uc/frzheng/zfr_files/jars/*
-hadoop fs -ls /user/uc/frzheng/zfr_files/jars/
-hadoop fs -put /home/frzheng/zfrFile/jars/* /user/uc/frzheng/zfr_files/jars/
-hadoop fs -ls /user/uc/frzheng/zfr_files/jars/
